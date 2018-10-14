86f666492322ca881859bc48af87f3254539474b
randomunrandom <randomdanilka@gmail.com>
2018-10-14 23:26:00 +0300

    finishing touches

--- alg.py
+++ alg.py
@@ -1,9 +1,9 @@
 def user_input():
-    res_E = list()
-    inp = int(input("введите количнство рёбер: "))
-    while inp <= 0:
-        inp = int(input("введите положительное число"))
-    for i in range(inp):
+    res_e = list()
+    ui_inp = int(input("введите количнство рёбер: "))
+    while ui_inp <= 0:
+        ui_inp = int(input("введите положительное число"))
+    for i in range(ui_inp):
         c = list(input("введите крайние вершины ребра {:^3} через запятую: ".format(i + 1)))
         if ',' in c:
             c.remove(',')
@@ -12,40 +12,40 @@ def user_input():
         c.sort()
         c = tuple(c)
         w = int(input("введите вес ребра {:^3}: ".format(i + 1)))
-        res_E.append(dict(coords=c, weight=w))
-    return res_E
+        res_e.append(dict(cords=c, weight=w))
+    return res_e
 
 
 def pre_input():
-    res_E = list()
-    res_E.append(dict(coords=(1, 2), weight=2))
-    res_E.append(dict(coords=(1, 4), weight=7))
-    res_E.append(dict(coords=(2, 3), weight=8))
-    res_E.append(dict(coords=(3, 4), weight=5))
-    res_E.append(dict(coords=(3, 4), weight=50))
-    res_E.append(dict(coords=(3, 5), weight=4))
-    res_E.append(dict(coords=(4, 5), weight=3))
-    res_E.append(dict(coords=(4, 6), weight=5))
-    res_E.append(dict(coords=(4, 8), weight=3))
-    res_E.append(dict(coords=(5, 8), weight=6))
-    res_E.append(dict(coords=(5, 9), weight=7))
-    res_E.append(dict(coords=(6, 7), weight=4))
-    res_E.append(dict(coords=(8, 9), weight=2))
+    res_e = list()
+    res_e.append(dict(cords=(1, 2), weight=2))
+    res_e.append(dict(cords=(1, 4), weight=7))
+    res_e.append(dict(cords=(2, 3), weight=8))
+    res_e.append(dict(cords=(3, 4), weight=5))
+    res_e.append(dict(cords=(3, 4), weight=50))
+    res_e.append(dict(cords=(3, 5), weight=4))
+    res_e.append(dict(cords=(4, 5), weight=3))
+    res_e.append(dict(cords=(4, 6), weight=5))
+    res_e.append(dict(cords=(4, 8), weight=3))
+    res_e.append(dict(cords=(5, 8), weight=6))
+    res_e.append(dict(cords=(5, 9), weight=7))
+    res_e.append(dict(cords=(6, 7), weight=4))
+    res_e.append(dict(cords=(8, 9), weight=2))
 
-    # res_E.append(dict(coords=(, ), weight=))
-    return res_E
+    # res_e.append(dict(cords=(, ), weight=))
+    return res_e
 
 
 def special_sort(list_of_points):
     p_lop = list()
     for i in range(len(list_of_points)):
-        if list_of_points[i]["coords"][0] != list_of_points[i]["coords"][1]:
+        if list_of_points[i]["cords"][0] != list_of_points[i]["cords"][1]:
             p_lop.append(list_of_points[i])
 
     list_to_pop = list()
     for i in range(len(p_lop)):
         for j in range(i):
-            if p_lop[i]["coords"] == p_lop[j]["coords"]:
+            if p_lop[i]["cords"] == p_lop[j]["cords"]:
                 list_to_pop.append(j)
                 p_lop[i]["weight"] = max(p_lop[i]["weight"], p_lop[j]["weight"])
 
@@ -61,29 +61,29 @@ def special_sort(list_of_points):
 def alg_Kraskala(list_of_points):
     res = list()
     buckets = list()
-    checked = False
     for l_i, l_el in enumerate(list_of_points):
         c0_b = False
         c1_b = False
-        w = l_el["weight"]
+        c0_i = -1
+        c1_i = -1
         for b_i, b_el in enumerate(buckets):
             b_el.sort()
-            if l_el["coords"][0] in b_el:
+            if l_el["cords"][0] in b_el:
                 c0_b = True
                 c0_i = b_i
-            if l_el["coords"][1] in b_el:
+            if l_el["cords"][1] in b_el:
                 c1_b = True
                 c1_i = b_i
-        if (c0_b == False) and (c1_b == False):
-            buckets.append([l_el["coords"][0], l_el["coords"][1]])
+        if (not c0_b) and (not c1_b):
+            buckets.append([l_el["cords"][0], l_el["cords"][1]])
             res.append(l_el)
-        elif (c0_b == False) and (c1_b == True):
-            buckets[c1_i].append(l_el["coords"][0])
+        elif (not c0_b) and c1_b:
+            buckets[c1_i].append(l_el["cords"][0])
             res.append(l_el)
-        elif (c0_b == True) and (c1_b == False):
-            buckets[c0_i].append(l_el["coords"][1])
+        elif c0_b and (not c1_b):
+            buckets[c0_i].append(l_el["cords"][1])
             res.append(l_el)
-        elif (c0_b == True) and (c1_b == True) and (c0_i != c1_i):
+        elif c0_b and c1_b and (c0_i != c1_i):
             buckets[c0_i] += buckets[c1_i]
             buckets.pop(c1_i)
             buckets[c0_i].sort()
@@ -93,15 +93,15 @@ def alg_Kraskala(list_of_points):
 
 def graph_print(list_of_points):
     for i in list_of_points:
-        print("ребро:", i["coords"], "\t, вес: ", i["weight"])
+        print("ребро:", i["cords"], "\t, вес: ", i["weight"])
 
 
 def graph_draw(list_of_points):
     import graphviz as gv
-    G = gv.Graph()
+    g = gv.Graph()
     for i in list_of_points:
-        G.edge(str(i["coords"][0]), str(i["coords"][1]), weight=str(i["weight"]))
-    G.render("Graph_{}".format(graph_num), view=True)
+        g.edge(str(i["cords"][0]), str(i["cords"][1]), weight=str(i["weight"]))
+    g.render("Graph_{}".format(graph_num), view=True)
     return
 
 
