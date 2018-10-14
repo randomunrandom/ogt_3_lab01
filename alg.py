6a47bd3af78bd688ee04207103996ffa31738e13
randomunrandom <randomdanilka@gmail.com>
2018-10-14 15:31:52 +0300

    initial commit

--- alg.py
+++ alg.py
@@ -2,12 +2,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from operator import attrgetter
 
-def ui():
-    te = list()
-    inp_e = int(input("введите количнство рёбер: "))
-    while inp_e <= 0:
-        inp_e = int(input("введите положительное число"))
-    for i in range(inp_e):
+
+def user_input():
+    res_E = list()
+    inp = int(input("введите количнство рёбер: "))
+    while inp <= 0:
+        inp = int(input("введите положительное число"))
+    for i in range(inp):
         c = list(input("введите крайние вершины ребра {:^3} через запятую: ".format(i + 1)))
         if ',' in c:
             c.remove(',')
@@ -16,26 +17,27 @@ def ui():
         c.sort()
         c = tuple(c)
         w = int(input("введите вес ребра {:^3}: ".format(i + 1)))
-        te.append(dict(coords=c, weight=w))
-    return te
+        res_E.append(dict(coords=c, weight=w))
+    return res_E
 
 
-def ps():
-    te = list()
-    te.append(dict(coords=(1, 1), weight=1))
-    te.append(dict(coords=(1, 2), weight=3))
-    te.append(dict(coords=(2, 3), weight=6))
-    te.append(dict(coords=(1, 3), weight=9))
-    te.append(dict(coords=(1, 3), weight=90))
-    te.append(dict(coords=(1, 4), weight=15))
-    te.append(dict(coords=(2, 4), weight=2))
-    te.append(dict(coords=(3, 4), weight=19))
-    te.append(dict(coords=(3, 4), weight=190))
-    # te.append(dict(coords=list(, ), weight=))
-    return te
+def pre_input():
+    res_E = list()
+    res_E.append(dict(coords=(1, 1), weight=1))
+    res_E.append(dict(coords=(1, 2), weight=3))
+    res_E.append(dict(coords=(2, 3), weight=6))
+    res_E.append(dict(coords=(1, 3), weight=9))
+    res_E.append(dict(coords=(1, 3), weight=90))
+    res_E.append(dict(coords=(1, 4), weight=15))
+    res_E.append(dict(coords=(2, 4), weight=2))
+    res_E.append(dict(coords=(3, 4), weight=19))
+    res_E.append(dict(coords=(3, 4), weight=190))
+    # res_E.append(dict(coords=list(, ), weight=))
+    return res_E
 
-def ss(list_of_points):
-    p_lop=list()
+
+def special_sort(list_of_points):
+    p_lop = list()
     for i in range(len(list_of_points)):
         if list_of_points[i]["coords"][0] != list_of_points[i]["coords"][1]:
             p_lop.append(list_of_points[i])
@@ -55,16 +57,34 @@ def ss(list_of_points):
 
     return p_lop
 
+
 def alg_Kraskala(list_of_points):  # TODO дописать реализацию алгоритма
     res = list()
-    if list_of_points == list():
-        return res
-
+    buckets = list()
+    checked = False
+    for i, el in enumerate(list_of_points):
+        for b in buckets:
+            if (el["coords"][0] not in b) and (el["coords"][1] in b):
+                checked = True
+                b.append(el["coords"][0])
+                res.append(el)
+            elif (el["coords"][0] in b) and (el["coords"][1] not in b):
+                checked = True
+                b.append(el["coords"][1])
+                res.append(el)
+            elif (el["coords"][0] in b) and (el["coords"][1] in b):
+                checked = True
+                pass
+        if checked == False:
+            buckets.append([el["coords"][0], el["coords"][1]])
+            res.append(el)
     return res
+
 
 def graph_print(list_of_points):
     for i in list_of_points:
         print("ребро:", i["coords"], "\t, вес: ", i["weight"])
+
 
 def graph_draw(list_of_points):  # TODO заюзать библиотеку для отрисовки граффов
     return
@@ -74,8 +94,8 @@ E = list()
 print("выберите способ задания графа одной из предложенных букв:\n\t-заранее: з\n\t-вручную: в")
 inp = input("введите выбраный способ: ")
 inp_options = {
-    'з': ps,
-    'в': ui
+    'з': pre_input,
+    'в': user_input
 }
 try:
     E = inp_options[inp]()
@@ -87,7 +107,7 @@ except KeyError as e:
 print("введённый граф:")
 graph_print(E)
 
-p_E = ss(E)
+p_E = special_sort(E)
 
 print("подготовленный граф:")
 graph_print(p_E)
